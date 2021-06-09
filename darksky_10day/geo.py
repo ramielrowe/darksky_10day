34ae824a979ab0b97339a3f62c286137e45e39ce
Andrew Melton <ramielrowe@gmail.com>
2021-06-09 16:56:22 -0400

    Use available geo dataset

--- darksky_10day/geo.py
+++ darksky_10day/geo.py
@@ -16,24 +16,24 @@ class Location(object):
 
 def zip_to_location(zipcode):
     params = {
-        'dataset': 'us-zip-code-latitude-and-longitude',
-        'q': zipcode,
+        'dataset': 'georef-united-states-of-america-zc-point',
+        'refine.zipcode': zipcode,
     }
     result = requests.get(ODS_SEARCH_URL, params=params).json()
     if result['nhits'] > 0:
         fields = result['records'][0]['fields']
-        name = '%s, %s' % (fields['city'], fields['state'])
-        return Location(name, fields['latitude'], fields['longitude'])
+        name = '%s, %s' % (fields['usps_city'], fields['ste_name'])
+        return Location(name, fields['geo_point_2d'][0], fields['geo_point_2d'][1])
 
 
 
 def lat_lon_to_location(lat, lon):
     params = {
-        'dataset': 'us-zip-code-latitude-and-longitude',
+        'dataset': 'georef-united-states-of-america-zc-point',
         'geofilter.distance': f'{lat},{lon},8000',
     }
     result = requests.get(ODS_SEARCH_URL, params=params).json()
     if result['nhits'] > 0:
         fields = result['records'][0]['fields']
-        name = '%s, %s' % (fields['city'], fields['state'])
-        return Location(name, fields['latitude'], fields['longitude'])
+        name = '%s, %s' % (fields['usps_city'], fields['ste_name'])
+        return Location(name, fields['geo_point_2d'][0], fields['geo_point_2d'][1])
